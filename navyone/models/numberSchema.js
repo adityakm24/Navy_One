f4c0f50e419231cf1ab9e7a220607f0a2fac5ad6
Sowrabh0-0 <sksseervi@gmail.com>
2023-11-20 00:01:47 +0530

    DB write Added

---
+++ navyone/models/numberSchema.js
@@ -0,0 +1,12 @@
+// models/number.js
+
+import mongoose from "mongoose";
+
+const numberSchema = new mongoose.Schema({
+  numbers: {
+    type: String,
+    required: true,
+  },
+});
+
+export const NumberModel = mongoose.models.number || mongoose.model("number", numberSchema);
--- navyone/pages/api/route.js
+++ navyone/pages/api/route.js
@@ -1,22 +1,24 @@
-import { connectionSrt , connectionSrt1  } from "@/models/connection";
-import { num } from "@/models/getnum";
 import mongoose from "mongoose";
-import { NextResponse } from "next/server";
+import { connectionSrt1 } from "@/models/connection";
+import { NumberModel } from "@/models/numberSchema"; // Import the NumberModel
 
 export default async function handler(req, res) {
   try {
     console.log("Attempting to connect to the database...");
-    await mongoose.connect(connectionSrt1); 
+    await mongoose.connect(connectionSrt1);
     console.log("DB Connected");
-    
-    console.log("Attempting to execute Demo.find()...");
-    const dat = await num.find()
-    console.log("Query executed successfully");
 
-    return res.json({ result: dat });
+    const newData = {
+      numbers: "12345", 
+    };
+
+    console.log("Attempting to insert data into number collection...");
+    const result = await NumberModel.create(newData);
+    console.log("Data inserted successfully:", result);
+
+    return res.json({ message: "Data inserted successfully", result });
   } catch (error) {
     console.error("Error handling API request:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 }
-
