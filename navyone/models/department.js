40e35ee572308fd98b590721c925a7ce5cae786e
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-11-19 19:53:55 +0530

    Update department.js

---
+++ navyone/models/department.js
@@ -0,0 +1,9 @@
+// models/department.js
+const mongoose = require("mongoose");
+
+const departmentSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+module.exports = mongoose.model("Department", departmentSchema);
