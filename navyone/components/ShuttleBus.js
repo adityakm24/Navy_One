b553bd771b4ee4452ee7e83c409a82a99f74fd8a
Gokul Sangeeth <askgokul123@gmail.com>
2023-11-20 00:47:53 +0530

    updated shuttlebus

--- navyone/components/ShuttleBus.js
+++ navyone/components/ShuttleBus.js
@@ -19,13 +19,19 @@ const ShuttleBus = () => {
         <div style={{
       border: '1px solid #000', // Border color
       padding: '4px', // Padding inside the box
-      margin: '15px 0 0 400px', // Margin around the box
+      margin: '15px 0 0 450px', // Margin around the box
       width: '180px' // Set a specific width
     }}>
       <p>START</p>
       <p>FINISH</p>
     </div>
-    
+    <div style={{
+      margin: '15px 0 0 450px'
+    }}>
+      <p>* ADDITIONAL SHUTTLE BUS SERVICE AS PER AVAILABILITY OF BUSES</p>
+    <p>Personnel Coming in between 2000-2300hrs may contact duty JCO at Travancore or Contact MT Pool on 9496450666 for transport requirements. The vehicle will be provided will be provided for personnel in groups as per availability.</p>
+    </div>
+   
         </div>
       </div>
 
