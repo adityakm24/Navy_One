b1eec9eac3ee7da4d79a93343a04870e3495fd27
Sowrabh0-0 <sksseervi@gmail.com>
2024-01-05 22:04:48 +0530

    changes

--- navyone/pages/_app.js
+++ navyone/pages/_app.js
@@ -1,4 +1,11 @@
 import "./global.css";
+import dotenv from "dotenv"
+
+require('dotenv').config({path:'./env'})
+
+// dotenv.config({
+//     path:'./env'
+// })
 
 function MyApp({ Component, pageProps }) {
   return (
